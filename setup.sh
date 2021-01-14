3643f1334623e5382cfc45115195e5f5d208ad79
fallenangel3k <fallenangel3k@googlemail.com>
2021-01-14 16:19:43 +0100

    Initial commit to provide new version.

---
+++ setup.sh
@@ -0,0 +1,11 @@
+sudo apt update
+sudo apt install perl gqrx libperl*json* libtest*cpan*
+
+sudo cpan App::cpanminus
+
+sudo cpanm GQRX::Remote
+sudo cpanm JSON
+sudo cpanm LWP::Simple
+sudo cpanm Net::Random
+sudo cpanm Crypt::Random
+
---
+++ startquantensinus.bat
@@ -0,0 +1,2 @@
+perl gqrx-ghostbox-mm-v3.pl --ghostmode 42 --ghostchance 7 -m random --sleep 1 --randsleep 1 --mysquelch -66
+
